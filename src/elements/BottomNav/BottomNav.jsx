a5a36323cac9fbeb1335a9840dd4cf072cf19a46
DevSheila <devsheilawambui@gmail.com>
2024-05-20 09:45:08 +0300

    add logout on bottom nav

--- src/elements/BottomNav/BottomNav.jsx
+++ src/elements/BottomNav/BottomNav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {useHistory} from 'react-router'
 import HomeIcon from '@material-ui/icons/Home'
 import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
@@ -12,8 +13,14 @@ import { useStateValue } from '../../contexts/StateContextProvider'
 
 const BottomNav = () => {
   const [{user}] = useStateValue()
+  const history = useHistory()
 
-
+  const signout = () => {
+    localStorage.clear()
+    // window.location.push('/')
+    history.push('/')
+    window.location.reload()
+}
   const text="profile";
   const profileLink = `${text.toLowerCase()}/${user.username}`;  
 
@@ -23,6 +30,8 @@ const BottomNav = () => {
           <NavLink exact to='/' activeClassName='bottomNav__active'><HomeIcon /><h2 className="bottomNav__text">Home</h2></NavLink>
           <NavLink to={`/${profileLink}`} activeClassName='bottomNav__active'><PermIdentityIcon /><h2 className="bottomNav__text">Profile</h2></NavLink>
           <a href="https://akurinuyouth.com/portal" activeClassName='bottomNav__active' target='_blank' rel='noopener noreferrer'><PanoramaFishEyeIcon /> <h2 className="bottomNav__text">Portal</h2></a>
+          <div onClick={signout} activeClassName='bottomNav__active'><PermIdentityIcon /><h2 className="bottomNav__text">Logout</h2></div>
+       
        </nav>
     </div>
   )
